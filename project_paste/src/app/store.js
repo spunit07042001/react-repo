57eb2881a5eaf14ad38d15929f2af1cd8a7ecb78
Punit Soni <85535330+spunit07042001@users.noreply.github.com>
2025-02-04 12:08:34 +0530

    Delete project_paste directory

--- project_paste/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import pasteReducer from '../features/paste/pasteSlice'
-
-export const store = configureStore({
-  reducer: {
-    paste : pasteReducer,
-  },
-})
